2d342ecf1ca2caece15ba51d62accb3dbb43a140
robertng0407 <robertng0407@gmail.com>
2019-02-23 13:27:02 -0800

    Setup editabletimer component

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView
+} from 'react-native';
+
+import EditableTimer from './src/components/EditableTimer';
+import ToggleableTimerForm from './src/components/ToggleableTimerForm';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+        <ScrollView style={styles.timerList}>
+          <ToggleableTimerForm isOpen={false} />
+          <EditableTimer
+            id="1"
+            title="Mow the lawn"
+            project="House chores"
+            elapsed="8986300"
+            isRunning
+          />
+          <EditableTimer
+            id="2"
+            title="Bake squash"
+            project="Kitchen chores"
+            elapsed="3890985"
+            isRunning
+          />
+        </ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    flex: 1
   },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: 'D6D7DA'
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  timerList: {
+    paddingBottom: 15
+  }
 });
---
+++ src/components/EditableTimer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Timer from './Timer';
+import TimerForm from './TimerForm';
+
+export default function EditableTimer({
+    id,
+    title,
+    project,
+    elapsed,
+    isRunning,
+    editFormOpen
+}) {
+    if (editFormOpen) {
+        return <TimerForm 
+            id={id} 
+            title={title}
+            project={project} />
+    }
+
+    return (
+        <Timer
+            id={id}
+            title={title}
+            project={project}
+            elapsed={elapsed}
+            isRunning={isRunning} />
+    );
+}
